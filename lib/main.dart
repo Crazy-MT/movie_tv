cbd309032481ef930f8d63cbcc8126e98da7134e
maotong <maotong@xiaoyezi.com>
2021-10-22 11:52:56 +0800

    更新次数

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:movie_tv/movie_tv.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'package:url_launcher/url_launcher.dart';
@@ -82,43 +83,11 @@ class _MyHomePageState extends State<MyHomePage> {
   List<Widget> children() {
     List<Widget> widgets = [];
     for (MeiJuMi meijumiSeries in meijumiSeriesList) {
-      widgets.add(GestureDetector(
-        child: Text(meijumiSeries.title),
-        onTap: () async {
-          setState(() {
-            meijumiSeries.response = "";
-          });
-          await meijumiSeries.getResponse();
-          setState(() {});
-        },
-      ));
-      widgets.add(Html(
-        data: meijumiSeries.response,
-        onLinkTap: (url, _, __, ___) {
-          // Clipboard.setData(ClipboardData(text: url));
-          launch(url);
-        },
-      ));
+      getMovieTvWidget(widgets, meijumiSeries);
     }
 
-    widgets.add(GestureDetector(
-      child: Text(dy2018.title),
-      onTap: () async {
-        setState(() {
-          dy2018.response = "";
-        });
-        await dy2018.getResponse();
-        setState(() {});
-      },
-    ));
-    widgets.add((Html(
-      data: dy2018.response,
-      onLinkTap: (url, _, __, ___) {
-        // Clipboard.setData(ClipboardData(text: url));
-        launch(dy2018.url + url);
-      },
-    )));
-    // print('MTMTMT _MyHomePageState.children ${Platform.localHostname} ');
+    getMovieTvWidget(widgets, dy2018);
+
     if (kIsWeb) {
       widgets.add(Text("To run web in Chrome\n"
           "1- Go to flutter\\bin\\cache and remove a file named: flutter_tools.stamp\n"
@@ -128,5 +97,25 @@ class _MyHomePageState extends State<MyHomePage> {
           "5- Add '--user-data-dir=/Users/mt/Desktop/chromedata'"));
     }
     return widgets;
+  }
+
+  getMovieTvWidget(List<Widget> widgets, MovieTv movieTv)  {
+    widgets.add(GestureDetector(
+      child: Text(movieTv.title + "   更新次数${movieTv.updateNum}"),
+      onTap: () async {
+        setState(() {
+          movieTv.response = "";
+        });
+        await movieTv.getResponse();
+        setState(() {});
+      },
+    ));
+    widgets.add(Html(
+      data: movieTv.response,
+      onLinkTap: (url, _, __, ___) {
+        // Clipboard.setData(ClipboardData(text: url));
+        launch(url.contains("http") ? url : movieTv.url + url);
+      },
+    ));
   }
 }
--- lib/movie_tv.dart
+++ lib/movie_tv.dart
@@ -2,20 +2,21 @@ import 'package:dio/dio.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:movie_tv/server_key.dart';
 
-class MovieTv{
-  String title;
-  String url;
+class MovieTv {
+  String title = "";
+  String url = "";
   String response = "";
+  int updateNum = 0;
 
   Future getResponse() async {
     Box lastResponseBox = await Hive.openBox<String>("movie_tv_box");
     String lastResponse = lastResponseBox.get(url, defaultValue: "");
-    if(lastResponse.isNotEmpty && lastResponse != response) {
+    if (lastResponse.isNotEmpty && lastResponse != response) {
       print('MTMTMT $title 有更新 ');
+      updateNum++;
       try {
         await Dio().get("https://sctapi.ftqq.com/$key.send?title=$title有更新");
-      }
-      catch(e) {}
+      } catch (e) {}
     }
 
     lastResponseBox.put(url, response); // 浏览器关闭之后数据会丢失
